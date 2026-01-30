b8cec2a09887bacad69e7db3d2ae9e065d679049
Md Saiful Islam <mdsaifulislam22203@gmail.com>
2026-01-31 01:34:54 +0600

    updated update

--- update.php
+++ update.php
@@ -7,6 +7,19 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $address = $_POST['address'];
 
+// Check duplicates excluding current ID
+$check = "SELECT * FROM contacts 
+          WHERE (phone='$phone' OR email='$email') AND id != $id";
+$result = mysqli_query($conn, $check);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<script>
+        alert('Phone number or Email already exists!');
+        window.location.href='edit.php?id=$id';
+    </script>";
+    exit();
+}
+
 $sql = "UPDATE contacts SET
         name='$name',
         phone='$phone',
