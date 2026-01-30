a212e959118bab37859babc50e7d85bfb6f29155
Md Saiful Islam <mdsaifulislam22203@gmail.com>
2026-01-31 01:34:15 +0600

    updated insert

--- insert.php
+++ insert.php
@@ -6,6 +6,19 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $address = $_POST['address'];
 
+// Check if phone or email already exists
+$check = "SELECT * FROM contacts WHERE phone='$phone' OR email='$email'";
+$result = mysqli_query($conn, $check);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<script>
+        alert('Phone number or Email already exists!');
+        window.location.href='add.php';
+    </script>";
+    exit();
+}
+
+// Insert if unique
 $sql = "INSERT INTO contacts (name, phone, email, address)
         VALUES ('$name', '$phone', '$email', '$address')";
 
